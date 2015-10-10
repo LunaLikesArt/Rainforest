dbdac0cc5693466ef9d115281935d3b6b5ee2ab8
Jenny Traer <lunathefuchs@gmail.com>
2015-10-10 11:50:38 -0400

    Initial Commit

---
+++ Rainforest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Rainforest
+//
+//  Created by Schinny on 10/10/15.
+//  Copyright © 2015 Schinny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
